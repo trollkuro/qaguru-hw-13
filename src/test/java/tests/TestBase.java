d107eb3fad2c5ed72260d51b8461758b3a47bf15
Kristina Egorova <kristi.egorova@gmail.com>
2023-11-04 21:22:06 +0300

    added remote configuration

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -14,6 +14,7 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
+        Configuration.remote ="https://user1:1234@selenoid.autotests.cloud/wd/hub";
         SelenideLogger.addListener("allure", new AllureSelenide());
     }
 
